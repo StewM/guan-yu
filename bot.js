36fb69e824901905f4af5097a90aa90805bc662a
Stewart Mink <stewart.mink@gmail.com>
2024-07-17 19:01:24 -0500

    add .5 sec wait before suppressing the embed

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const Check = require('./Check.js');
 const Dracula =  require('./Dracula.js');
 const CON = require('./constants.js');
 const UTIL = require('./utilities.js');
+import { setTimeout } from "timers/promises";
 
 const pattern = /(\|\|(?:\s*)?)?https?:\/\/(?:www\.)?(?:twitter|x)\.com\/([\w_]+\/status\/\d+)(?:\?\S+)?((?:\s*)?\|\|)?/;
 
@@ -313,6 +314,7 @@ module.exports = {
 			let channel = message.channel;
 			await channel.send(reply);
 			try{
+				await setTimeout(500);
 				await message.suppressEmbeds(true);
 			}
 			catch(err) {
